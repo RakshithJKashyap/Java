40ce9ed1f927f6318915a9dabe2dfff66fe31fe1
Rakshith J <rakshithj07@gmail.com>
2021-05-02 18:54:33 +0530

    New Files

---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int sum;
+        sum=a+b;
+        System.out.println("A is "+a+"\nB is "+b+"\nSum is "+sum );
+    }
+    
+}
